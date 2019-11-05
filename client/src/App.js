1cac42afce1d836326e359930ac2659301254080
Mirwais Nezami <mirwais79@hotmail.com>
2019-11-05 15:18:34 +0100

    the content for log in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
-import LoginPage from './components/loginPage';
+import './App.scss';
+import './styles.scss';
+import LogInPage from './components/logInPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <LogInPage></LogInPage>
       </header>
     </div>
   );
--- client/src/components/loginPage.js
+++ client/src/components/loginPage.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
+import {
+    Button, Input,
+    Form,
+    Row,
+    Col,
+    FormGroup,
+    Label
+} from 'reactstrap';
 
-function LoginPage() {
+function LogInPage() {
 
 
 
     return (
-        <div>
-
-            <InputGroup>
-                <Input />
-                <InputGroupAddon addonType="append">
-                    <Button color="secondary">To the Right!</Button>
-                </InputGroupAddon>
-            </InputGroup>
-        </div>
+        <div className="container">
+            <div className="logInContent mt-5">
+                <div className="logInHeader">
+                    <Row>
+                        <Col lx={6} lg={6} md={6} sm={6} >
+                            <h2>Välkommen till Bling</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lx={6} lg={6} md={6} sm={6} >
+                            <h4>Vänligen logga in på ditt konto.</h4>
+                        </Col>
+                    </Row>
+                </div>
+                <Form>
+                    <Row form>
+                        <Col lx={6} lg={6} md={6} sm={6}>
+                            <FormGroup>
+                                <Label for="exampleEmail">Email</Label>
+                                <Input type="email" name="email" id="exampleEmail" placeholder="Ange din email här" />
+                            </FormGroup>
+                        </Col>
+                        <Col lx={6} lg={6} md={6} sm={6}>
+                            <FormGroup>
+                                <Label for="examplePassword">Lösenord</Label>
+                                <Input type="password" name="password" id="examplePassword" placeholder="Ange ditt lösenord här" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lx={6} lg={6} md={6} sm={6}>
+                            <Button className="logInBtn mr-3">Logga in</Button>
+                            <Button className="forgotPasswordBtn mr-3">Glömt lösenord</Button>
+                            <Button className="newUserBtn">Registrera dig som ny användare</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </div >
+        </div >
     );
-
-
 };
 
-export default LoginPage;
+export default LogInPage;
