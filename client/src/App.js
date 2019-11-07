030e6bc9fd6cd110cfd85f465f0b09940dd91c1b
Mirwais Nezami <mirwais79@hotmail.com>
2019-11-07 10:54:40 +0100

    Bug fix at log in page

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LogInPage></LogInPage>
+        <LogInPage />
       </header>
     </div>
   );
