522d7adc7ddb7e7a5d4ca6cff47cf5fd1592e663
Mirwais Nezami <mirwais79@hotmail.com>
2019-11-07 11:15:46 +0100

    bug fix on commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Navbar from './components/NavBar';
 import './App.scss';
 import './styles.scss';
 import LogInPage from './components/logInPage';
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <main>
           <Switch className="switch">
             <Route path="/" exact component={LogInPage} />
