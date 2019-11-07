24a4d2f4840e0c8a16c06d22b4d6bd6843c694c3
Mirwais Nezami <mirwais79@hotmail.com>
2019-11-07 10:46:37 +0100

    Log in page added

--- client/src/components/loginPage.js
+++ client/src/components/loginPage.js
@@ -17,36 +17,38 @@ function LogInPage() {
             <div className="logInContent mt-5">
                 <div className="logInHeader">
                     <Row>
-                        <Col lx={6} lg={6} md={6} sm={6} >
-                            <h2>Välkommen till Bling</h2>
+                        <Col lx={12} lg={12} md={12} sm={6} >
+                            <h2 className="h2LogInHeader">Välkommen till Bling</h2>
                         </Col>
                     </Row>
                     <Row>
-                        <Col lx={6} lg={6} md={6} sm={6} >
-                            <h4>Vänligen logga in på ditt konto.</h4>
+                        <Col lx={12} lg={12} md={12} sm={6} >
+                            <h4 className="h4LogInHeader">Vänligen logga in på ditt konto</h4>
                         </Col>
                     </Row>
                 </div>
                 <Form>
                     <Row form>
-                        <Col lx={6} lg={6} md={6} sm={6}>
+                        <Col lx={12} lg={12} md={12} sm={6}>
                             <FormGroup>
-                                <Label for="exampleEmail">Email</Label>
+                                <Label for="emailLabel">Email</Label>
                                 <Input type="email" name="email" id="exampleEmail" placeholder="Ange din email här" />
                             </FormGroup>
-                        </Col>
-                        <Col lx={6} lg={6} md={6} sm={6}>
                             <FormGroup>
-                                <Label for="examplePassword">Lösenord</Label>
+                                <Label for="passwordLabel">Lösenord</Label>
                                 <Input type="password" name="password" id="examplePassword" placeholder="Ange ditt lösenord här" />
                             </FormGroup>
                         </Col>
                     </Row>
                     <Row>
-                        <Col lx={6} lg={6} md={6} sm={6}>
-                            <Button className="logInBtn mr-3">Logga in</Button>
-                            <Button className="forgotPasswordBtn mr-3">Glömt lösenord</Button>
-                            <Button className="newUserBtn">Registrera dig som ny användare</Button>
+                        <Col lx={12} lg={12} md={12} sm={6}>
+                            <Button color="success" className="logInBtn mr-3">Logga in</Button>
+                        </Col>
+                        <Col lx={12} lg={12} md={12} sm={6}>
+                            <Button color="info" className="forgotPasswordBtn mr-3">Glömt lösenord</Button>
+                        </Col>
+                        <Col lx={12} lg={12} md={12} sm={6}>
+                            <Button color="primary" className="newUserBtn">Registrera dig som ny användare</Button>
                         </Col>
                     </Row>
                 </Form>
