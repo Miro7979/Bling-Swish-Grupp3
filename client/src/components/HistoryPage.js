2503fd0181b70d85f9faf5c6754e93fd88263d3b
sunsneezer <xil_wong@hotmail.com>
2019-11-13 09:57:29 +0100

    dropdown is now a component in historyPage, also added scss-files

--- client/src/components/HistoryPage.js
+++ client/src/components/HistoryPage.js
@@ -1,48 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React from 'react';
+import HistoryDropdown from './HistoryDropdown.js';
 
 function HistoryPage() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [myChildren, setMyChildren] = useState([]);
-  const [dropdownTitle, setDropdownTitle] = useState('Min historik');
-
-
-  useEffect(() => {
-
-    function fetchChildren() {
-      //Dummy data, should be replaced later when db is available.
-      let arrayWithMyChildren = ['Lisa', 'Emma', 'Hannes'];
-      setMyChildren(arrayWithMyChildren);
-    }
-
-    fetchChildren();
-  }, [setMyChildren]);
-
-  const toggle = () => setDropdownOpen( prevState => !prevState);
-
-  const displayDropdownTitle = (e) => {
-
-    setDropdownTitle(e.target.value);
-
-  }
 
   return (
     <div className="container history-page">
-      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-        <DropdownToggle caret>
-          {dropdownTitle}
-        </DropdownToggle>
-        <DropdownMenu>
-          { dropdownTitle != 'Min historik' ? 
-            <DropdownItem onClick={e => displayDropdownTitle(e)} value={'Min historik'}>Min historik</DropdownItem>
-            : null }
-          { myChildren.map( (child, id) => (
-            dropdownTitle != child ? 
-            <DropdownItem onClick={e => displayDropdownTitle(e)} value={child} key={id}>{child}</DropdownItem>
-            : null
-          ))}
-        </DropdownMenu>
-      </Dropdown>
+      <HistoryDropdown />
     </div>
   );
 }
