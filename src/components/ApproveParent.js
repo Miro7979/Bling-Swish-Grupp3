a35981ef26d6e7f9ed5d54b1449aca383ed27cdb
Mirwais Nezami <mirwais79@hotmail.com>
2019-12-17 15:57:58 +0100

    update the Dev

--- src/components/ApproveParent.js
+++ src/components/ApproveParent.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Modal, ModalBody, ModalFooter, Alert } from 'reactstrap';
 import { Link } from "react-router-dom";
-
 import { Approveparent } from 'the.rest/dist/to-import';
 
 
